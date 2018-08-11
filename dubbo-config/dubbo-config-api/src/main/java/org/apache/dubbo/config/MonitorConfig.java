f7bbd0af53c1b3079d295ce4a6039293373521fd
rex <rex19951001@gmail.com>
2018-08-11 21:43:56 +0800

    监控中心解读

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/MonitorConfig.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/MonitorConfig.java
@@ -22,17 +22,27 @@ import java.util.Map;
 
 /**
  * MonitorConfig
- *
+ * 监控中心配置
  * @export
  */
 public class MonitorConfig extends AbstractConfig {
 
     private static final long serialVersionUID = -1184681514659198203L;
-
+    /**
+     * 监控中心协议，
+     * 如果为protocol="registry"，表示从注册中心发现监控中心地址，
+     * 否则直连监控中心
+     */
     private String protocol;
 
+    /**
+     * 直连监控中心服务器地址
+     */
     private String address;
 
+    /**
+     * 剩下的省略。。。。
+     */
     private String username;
 
     private String password;
